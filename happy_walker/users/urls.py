d5111d6f2769c41df04c09c102cc3148a848af6f
Illia Boichuk <ilia.boichuk@gmail.com>
2018-08-20 16:08:33 +0300

    implement get user profile API

--- happy_walker/users/urls.py
+++ happy_walker/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import UserRegister, UserLogin, ConfirmEmail
+from .views import UserRegister, UserLogin, ConfirmEmail, Profile
 from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
     path('register', csrf_exempt(UserRegister.as_view()), name='register'),
     path('confirm_email', csrf_exempt(ConfirmEmail.as_view()), name='confirm_email'),
+    path('<user_id>', csrf_exempt(Profile.as_view()), name='profile'),
     path('sign-in', UserLogin.as_view(), name='login'),
 ]
