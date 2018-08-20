533897cf2842470f8aaaf358371040b4c3e6f24d
Illia Boichuk <ilia.boichuk@gmail.com>
2018-08-20 16:21:11 +0300

    add decorator login_required

--- happy_walker/users/urls.py
+++ happy_walker/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import UserRegister, UserLogin, ConfirmEmail, Profile
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
     path('register', csrf_exempt(UserRegister.as_view()), name='register'),
     path('confirm_email', csrf_exempt(ConfirmEmail.as_view()), name='confirm_email'),
-    path('<user_id>', csrf_exempt(Profile.as_view()), name='profile'),
+    path('<user_id>', login_required(csrf_exempt(Profile.as_view())), name='profile'),
     path('sign-in', UserLogin.as_view(), name='login'),
 ]
