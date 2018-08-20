49a79161bf776af7ce2d9b54555f2aa50ba9c88e
zOFsky <zfstempor@gmail.com>
2018-08-20 11:47:22 +0300

    Working version with code refactoring for validator

--- happy_walker/users/urls.py
+++ happy_walker/users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import UserRegister, UserLogin, UserUpdateProfile
+from .views import UserRegister, UserLogin
 from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
     path('register', csrf_exempt(UserRegister.as_view()), name='register'),
     path('sign-in', UserLogin.as_view(), name='login'),
-    path('me', UserUpdateProfile.as_view(), name='update'),
 ]
