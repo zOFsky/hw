c050cb4e3459e179e75b05b8b624d130c09755be
zOFsky <zfstempor@gmail.com>
2018-08-30 17:05:37 +0300

    added STATIC_ROOT in settings.py

--- happy_walker/happy_walker/settings.py
+++ happy_walker/happy_walker/settings.py
@@ -101,6 +101,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
