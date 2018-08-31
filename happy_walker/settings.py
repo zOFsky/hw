5ef844d36be982c354bd37bc800389d1170feb75
zOFsky <zfstempor@gmail.com>
2018-08-31 13:08:33 +0300

    updating branch

--- happy_walker/settings.py
+++ happy_walker/settings.py
@@ -1,5 +1,5 @@
 import os
-#import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -109,4 +109,4 @@ STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Activate Django-Heroku.
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
