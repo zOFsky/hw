43528c0a5d56d2a05fa2d0b377d95580b8e64573
zOFsky <zfstempor@gmail.com>
2018-08-31 11:14:14 +0300

    new git init location

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM alpine
-RUN apk add python3
-RUN mkdir /data
-WORKDIR /data
-ADD requirements.txt /data
-RUN pip3 install -r requirements.txt
-ADD . /data
--- happy_walker/settings.py
+++ happy_walker/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -106,3 +107,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
