d3fb95bf1e00393c2d24d2997d217ff5642982cd
zOFsky <zfstempor@gmail.com>
2018-09-07 17:27:44 +0300

    deleted useless test.py

--- happy_walker/justtest.py
+++
@@ -1,5 +0,0 @@
-# test.py
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello HURRAY!!!! WORLD!!!!"] # python3
-    #return ["Hello World"] # python2
