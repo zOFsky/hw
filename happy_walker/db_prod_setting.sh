ef1351e5d636aec9a43e14ffd0d25ae70ed015bf
Illia Boichuk <ilia.boichuk@gmail.com>
2018-09-14 16:20:23 +0300

    fix

--- happy_walker/db_prod_setting.sh
+++ happy_walker/db_prod_setting.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DB_NAME='heroku_bcnr4g6v'
-DB_HOST='ds245512.mlab.com'
-DB_PORT=45512
-DB_USER='User'
-DB_PASS='qwerty123'
-AUTH_SOURCE='heroku_bcnr4g6v'
+export DB_NAME=heroku_bcnr4g6v
+export DB_HOST=ds245512.mlab.com
+export DB_PORT=45512
+export DB_USER=User
+export DB_PASS=qwerty123
+export AUTH_SOURCE=heroku_bcnr4g6v
--- happy_walker/db_test_setting.sh
+++ happy_walker/db_test_setting.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 export DB_NAME=mongodb
-export DB_HOST=
+export DB_HOST=""
 export DB_PORT=27017
-export DB_USER=
-export DB_PASS=
-export AUTH_SOURCE=
+export DB_USER=""
+export DB_PASS=""
+export AUTH_SOURCE=""
+
