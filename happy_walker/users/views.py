200e602aa68a6d5f701244be4fcb30684ceefbb5
Illia Boichuk <ilia.boichuk@gmail.com>
2018-09-18 11:10:04 +0300

    fix email callback

--- happy_walker/users/views.py
+++ happy_walker/users/views.py
@@ -72,6 +72,7 @@ class UserRegisterView(View):
             context = {
                 'uid': user.id,
                 'token': token_generator.make_token(user),
+                'host': request.META['HTTP_HOST'],
             }
             email = user.email
             mail_subject = 'Activate your HappyWalker account'
