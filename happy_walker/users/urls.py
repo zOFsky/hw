9338eb6c51b35799edb522cdd2d16b46f4b9ce0e
zOFsky <zfstempor@gmail.com>
2018-09-21 15:38:20 +0300

    before static check

--- happy_walker/users/urls.py
+++ happy_walker/users/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('sign_in', csrf_exempt(UserLoginView.as_view()), name='login'),
     path('change_password', login_required(csrf_exempt(ChangePasswordView.as_view())), 
                   name='change_password'),
+    path('logout', UserLogoutView.as_view(), name='logout'),
     path('forgot_password', csrf_exempt(ForgotPasswordView.as_view()), name='forgot_password'),
     path('reset_password', csrf_exempt(ResetPasswordView.as_view()), name='reset_password'),
     path('upload_photo', csrf_exempt(login_required(UploadPhotoView.as_view())), name='image'),
