f320012ede2c324ee6a0ed12a771a716e17b158a
Illia Boichuk <ilia.boichuk@gmail.com>
2018-09-27 16:44:09 +0300

    test google api

--- happy_walker/happy_walker/settings.py
+++ happy_walker/happy_walker/settings.py
@@ -96,6 +96,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
--- happy_walker/happy_walker/urls.py
+++ happy_walker/happy_walker/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from users.views import Oauth2Callback
 
 urlpatterns = [
     path('', include('home.urls')),
     path('users/', include('users.urls')),
+    path('oauth2callback', Oauth2Callback.as_view(), name='oauth2callback'),
 ]
