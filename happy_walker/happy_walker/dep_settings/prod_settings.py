e4d24729d7b2000557c92c33d150244076fc2a12
Illia Boichuk <ilia.boichuk@gmail.com>
2018-10-17 14:00:35 +0300

    fix google auth

--- happy_walker/happy_walker/dep_settings/prod_settings.py
+++ happy_walker/happy_walker/dep_settings/prod_settings.py
@@ -5,6 +5,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 REDIRECT_URI = os.environ['REDIRECT_URI']
+REDIRECT_URI_CRED = os.environ['REDIRECT_URI_CRED']
 DOMEN = os.environ['DOMEN']
 
 ALLOWED_HOSTS = ['a-qa-backend-happy-walker.herokuapp.com', 'a-prod-backend-happy-walker.herokuapp.com']
